0fb5884132711772b3158f451831474c8d90d3a2
Yuioppe <yuiop1620@hotmail.co.uk>
2014-05-25 02:33:54 +0100

    Redid preset colors based off of standard terminal colourschemes

--- src/screen.h
+++ src/screen.h
@@ -1,15 +1,24 @@
 #ifndef _SCREEN_H_
 #define _SCREEN_H_
 
-#define COL_BLACK   0x000000
-#define COL_WHITE   0xffffff
-#define COL_RED     0xff0000
-#define COL_GREEN   0x00ff00
-#define COL_BLUE    0x0000ff
-#define COL_MAGENTA 0xff00ff
-#define COL_YELLOW  0xffff00
-#define COL_CYAN    0x00ffff
-#define COL_TRANS   COL_MAGENTA
+#define COL_BLACK       0x000000
+#define COL_RED         0x7f0000
+#define COL_GREEN       0x007f00
+#define COL_YELLOW      0x7f7f00
+#define COL_BLUE        0x00007f
+#define COL_MAGENTA     0x7f007f
+#define COL_CYAN        0x007f7f
+#define COL_WHITE       0xcfcfcf
+#define COL_BRBLACK     0x7f7f7f
+#define COL_BRRED       0xff0000
+#define COL_BRGREEN     0x00ff00
+#define COL_BRYELLOW    0xffff00
+#define COL_BRBLUE      0x0000ff
+#define COL_BRMAGENTA   0xff00ff
+#define COL_BRCYAN      0x00ffff
+#define COL_BRWHITE     0xffffff
+#define COL_TRANS       COL_BRMAGENTA
+#define COL_GREY        COL_BRBLACK
 
 int  initScreen(void);
 void cleanupScreen(void);
