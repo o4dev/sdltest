65eb1b6ed4fe3e51c60a852880b7d2bbd04cb395
Yuioppe <yuiop1620@hotmail.co.uk>
2014-05-25 13:47:50 +0100

    Another test bitmap

--- src/sdltest.c
+++ src/sdltest.c
@@ -23,15 +23,21 @@ void tick(void) {
     const unsigned char *kbstate = SDL_GetKeyboardState(NULL);
     if (kbstate[SDL_SCANCODE_ESCAPE])
         running = FALSE;
-    
 }
 
-#include "face"
+#include "bitmaps/boxes"
 
 void render(void) {
-    clear(COL_BRWHITE);
+    for (int i = 0; i != WIDTH * HEIGHT; i++)
+        setPixel(i, 0, i + cticks);
+    clear(COL_WHITE);
     drawStr(9, 9, "Hello, World!", COL_GREY);
     drawStr(8, 8, "Hello, World!", COL_RED);
+    int w = BOXES_WIDTH  * 5 + (int)(cos(cticks / 500.0) * 10);
+    int h = BOXES_HEIGHT * 5 + (int)(sin(cticks / 500.0) * 10);
+    int x = (WIDTH  - w) / 2;
+    int y = (HEIGHT - h) / 2;
+    drawImg(x, y, w, h, BOXES_WIDTH, BOXES_HEIGHT, BOXES);
 }
 
 void run(void) {
